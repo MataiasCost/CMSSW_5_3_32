fcf9ae321c6e1ace74650e6453a2391d461c4c7b
MataiasCost <37384431+MataiasCost@users.noreply.github.com>
2020-06-03 16:34:59 -0300

    Add files via upload

---
+++ Analyzer_offline/THSTACKES/Mass_Inva_ee_mumu.C
@@ -0,0 +1,82 @@
+{
+#include<math.h>
+#include<TMath.h>
+#include <TLorentzVector.h>
+#include <TChain.h>
+//using namespace std;
+
+
+TCanvas *c1=new TCanvas("c1","Tudo empilhado",200,10,600,480);
+
+THStack *hs = new THStack("hs", "CMS 2.3 fb^{-1} at #sqrt{s} = 7 TeV");
+
+TFile f1("ht_ttbar.root");
+TH1F * h1 = new TH1F("h1","MassInva" , 50,0,350);
+h1 = (TH1F*)f1.Get("MassInva (ee,mumu)");
+h1->SetFillColor(2);
+
+TFile f2("ht_DY_M50.root");
+TH1F * h2 = new TH1F("h2","MassInva" , 50,0,350);
+h2 = (TH1F*)f2.Get("MassInva (ee,mumu)");
+h2->SetFillColor(7);
+
+TFile f3("ht_DY_M10_50.root");
+TH1F * h3 = new TH1F("h3","MassInva" , 50,0,350);
+h3 = (TH1F*)f3.Get("MassInva (ee,mumu)");
+h3->SetFillColor(kCyan+1);
+
+TFile f4("ht_TW.root");
+TH1F * h4 = new TH1F("h4","MassInva" , 50,0,350);
+h4 = (TH1F*)f4.Get("MassInva (ee,mumu)");
+h4->SetFillColor(41);
+
+TFile f5("ht_VV.root");
+TH1F * h5 = new TH1F("h5","MassInva" , 50,0,350);
+h5 = (TH1F*)f5.Get("MassInva (ee,mumu)");
+h5->SetFillColor(3);
+
+TFile f6("ht_data.root");
+TH1F * h6 = new TH1F("h6","MassInva" , 50,0,350);
+h6 = (TH1F*)f6.Get("MassInva (ee,mumu)");
+h6->SetFillColor(1);
+h6->SetMarkerStyle(kFullCircle);
+
+
+TLegend* leg2 = new TLegend(0.6,0.6,0.8,0.8);
+leg2->AddEntry(h6,"data","AP");
+leg2->AddEntry(h1,"t#bar{t} signal","f");
+leg2->AddEntry(h2,"DY_M50","f");
+leg2->AddEntry(h3,"DY_M10_50","f");
+leg2->AddEntry(h4,"TW","f");
+leg2->AddEntry(h5,"VV","f");
+
+
+leg2->SetBorderSize(0);
+TLatex *tx =new TLatex();
+tx->SetTextSize(0.08);
+tx->SetTextAlign(13);
+tx->SetTextFont(42);
+
+
+
+
+hs->Add(h1);
+hs->Add(h4);
+hs->Add(h5);
+hs->Add(h2);
+hs->Add(h3);
+hs->SetMaximum(1400);
+hs->Draw("HIST");
+h6->Draw("same AP0E");
+leg2->Draw();
+
+hs->GetXaxis()->SetTitle("M_{ll} [GeV]");
+hs->GetXaxis()->SetTitleSize(0.06);
+hs->GetXaxis()->SetTitleOffset(0.75);
+hs->GetYaxis()->SetTitle("Entries/ 7 GeV");
+hs->GetYaxis()->SetTitleSize(0.06);
+hs->GetYaxis()->SetTitleOffset(0.8);
+tx->DrawLatexNDC(0.15,0.85,Form("#mu#mu, ee"));
+c1->SaveAs("MassInva(ee,mumu).png");
+
+}
---
+++ Analyzer_offline/THSTACKES/second.sh
@@ -0,0 +1,14 @@
+#/bin/bash
+
+root -l -q Mass_Inva_emu.C
+root -l -q Mass_Inva_ee_mumu.C
+root -l -q MET_ee_mumu.C
+root -l -q MET_emu.C
+root -l -q NJetB_ee_mumu.C
+root -l -q NJetB_emu.C
+root -l -q NJet_ee_mumu.C
+root -l -q NJet_emu.C
+root -l -q PtEle.C
+root -l -q Pt_Jet.C
+root -l -q PtMuon.C
+
